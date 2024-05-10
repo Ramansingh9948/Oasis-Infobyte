3ed5598c39517a6a401597acc704a9bf5e3a73d1
Raman Singh <rsingh24595@gmail.com>
2024-05-10 11:38:59 +0530

    To Do Web App

---
+++ index.js
@@ -0,0 +1,39 @@
+const input = document.getElementById("input");
+const list = document.getElementById("listcontainer");
+
+function addTask() {
+    if(input.value=== '') {
+        alert("No task Written");
+    }
+    else {
+        let li= document.createElement("li");
+            li.innerHTML = input.value;
+            list.appendChild(li);
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
+    }
+    input.value ='';
+    saveData();
+}
+
+list.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("task");
+        saveData();
+        
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+function saveData() {
+    localStorage.setItem("data", list.innerHTML);
+}
+function showTask() {
+    list.innerHTML= localStorage.getItem("data");
+}
+showTask();
